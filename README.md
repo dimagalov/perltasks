1905a3e49b6d2f20525f571adbfef54f4e8fbf21
Dima Galov <dimagalov@gmail.com>
2015-09-29 23:58:29 +0400

    First task is done!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # perltasks
 Repository for Perl homework (mail.ru open course)
+
+I'm /usr/bin/perl, v5.14.2, on linux!
---
+++ part1/task1.pl
@@ -0,0 +1,1 @@
+ls -l | perl -nlae '$total += 1; if( $F[4] >= 1024 * 1024 ){ print "- ".join(" ", @F[8..$#F]); $big += 1 } END { print "Total files: $total | Big files: $big" }'
