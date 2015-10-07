53e81ea47bc7d1c8f94c06ced04ee4cbd1252267
Dima Galov <dimagalov@gmail.com>
2015-10-07 17:40:51 +0400

    Perl code style fixed

--- part1/task1.pl
+++ part1/task1.pl
@@ -1,1 +1,3 @@
-ls -l | perl -nlae '$total += 1; if( $F[4] >= 1024 * 1024 ){ print "- ".join(" ", @F[8..$#F]); $big += 1 } END { print "Total files: $total | Big files: $big" }'
+#!/usr/bin/env perl -nla
+
+$total += 1; if ( $F[4] >= 1024 * 1024 ) { print "- ".join(" ", @F[8..$#F]); $big += 1 } END { print "Total files: $total | Big files: $big" }
--- part1/task2.pl
+++ part1/task2.pl
@@ -1,1 +1,3 @@
-perl -nlaF':' -e '$row += 1; $column = 1; for( @F ){ if( $_ > 10 ){ print "Row: $row | Column: $column | Value: $_" }; $column += 1 }'
+#!/usr/bin/env perl -nlaF':'
+
+$row += 1; $column = 1; for ( @F ) { if ( $_ > 10 ) { print "Row: $row | Column: $column | Value: $_" }; $column += 1 }
